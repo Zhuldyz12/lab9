12bfbc5d3d37c1b33be263fc5c2286aae3658196
Zhuldyz12 <111428693+Zhuldyz12@users.noreply.github.com>
2022-10-25 21:56:49 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function HelloWorld(a) {console.log(a);
+    console.log(a);
+    console.log(a);}
+HelloWorld("Hello, World!"); 
+
+function Check(s) {
+    if (s<1 && s>0) {console.log("the number is between 0 and 1");}
+    else {console.log("the number is NOT between 0 and 1");}
+}
+Check(0.5);
+Check(2);
+
+function Equal(q,w) {
+    if (q === w) {console.log("they are equal");}
+    else {console.log("they are not equal")}
+}
+Equal(13,14);
+Equal(18,18);
